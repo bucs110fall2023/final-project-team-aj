5eb8f2729510fa3ebd143790c7c94bf82dd3e2cd
Aaron Damsky <aarondamsky@gmail.com>
2023-11-27 18:16:42 -0500

    Update 6:16 PM 11/27

---
+++ src/controller.py
@@ -0,0 +1,18 @@
+from ball import Ball
+
+class Controller:
+    
+    def __init__(self):
+        pass
+    
+    def startscreenloop(self):
+        pass
+    
+    def gameloop(self):
+        pass
+    
+    def endscreenloop(self):
+        pass
+    
+    def mainloop(self):
+        pass
